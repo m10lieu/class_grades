b3e2e3670d1cc9a7b49420b7faa122f59229463a
m10lieu <m10lieu@siena.edu>
2015-12-08 15:56:23 -0500

    Update adas_code.py

--- adas_code.py
+++ adas_code.py
@@ -3,7 +3,7 @@ import numpy as np
 def read_data(infile):
     columns= np.loadtxt('infile',delimiter=',',skiprows=0,unpack=True)
     grade_column= columns[1]
-    return columns, grade_columns
+    return grade_column
 
 
     
